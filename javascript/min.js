af7a5dacb969ffcb4a619af10609dbe5e7bca45c
Smriti Grg <gurungsm.10@gmail.com>
2024-08-04 10:59:59 +0545

    first commit

---
+++ javascript/min.js
@@ -0,0 +1,136 @@
+// document.getElementById("demo").innerHTML = "Hello World";
+
+// var x = document.getElementsByClassName("new");
+// document.getElementById("new").innerHTML = 
+//     x[0].innerHTML + x[1].innerHTML + x[2].innerHTML;
+
+// document.getElementsByTagName("h1")[0].innerHTML = "Hello World it's h1.";
+
+// document.querySelector("#demo").innerHTML = "Hello World";
+// document.querySelector(".new").innerHTML = "Hello World";
+
+// document.querySelectorAll(".new").innerHTML = "Hello World";
+
+// var x = 10;
+// var y = 15;
+// if (x < y){
+//     document.write("x is less than y.");
+// }
+
+// var x = 10;
+// var y = 15;
+// if (x < y){
+//     document.write("x is less than y.");
+// }
+// else{
+//     document.write("y is greater than x.");
+// }
+
+// var x = 10;
+// var y = 15;
+// var z = 5;
+// if (x > y && x > z){
+//     document.write("x is greater.");
+// }
+// else if(y > z && y > x){
+//     document.write("Y is greater.");
+// }
+// else{
+//     document.write("Z is greater");
+// }
+
+// var x = 3;
+// var y = 4;
+// var z = 5;
+// if(x < y){
+//     if(y > z){
+//         document.write("Y is greater.");
+//     }
+//     else{
+//         document.write("Z is greater.")
+//     }
+// }
+// else{
+//     document.write("Z is greater.")
+// }
+
+
+// var i = 1;
+// while(i <= 10){
+//     document.write(i);
+//     i++;
+// }
+
+// do{
+//     document.write(i);
+//     i++;
+// }
+// while(i <= 10)
+
+// for (i = 1; i <= 10; i++){
+//     document.write(i);
+// }
+
+
+// var day;
+// switch(new Date().getDay()){
+//     case 0:
+//     day = "Sunday";
+//     break;
+//     case 1:
+//         day = "Monday";
+//         break;
+//     case 2:
+//         day = "Tuesday";
+//         break;
+//     case 3:
+//         day = "Wednesday";
+//         break;
+//     case 4:
+//         day = "Thursday";
+//         break;
+//     case 3:
+//         day = "Friday";
+//         break;
+//     case 4:
+//         day = "Saturday";
+//         break;
+// }
+// document.write(day);
+// document.write("<br>"+ new Date().getDay());
+// document.write("<br>"+ new Date().getMonth());
+// document.write("<br>"+ new Date().getFullYear());
+
+// function newDate(){
+//     document.getElementById("date").innerHTML = "This is on click event";
+// }
+
+// function onmouse(){
+//     var x = document.getElementById("jscss");
+//     x.style.backgroundColor = "red";
+//     x.style.padding = "1rem";
+// }
+
+// function onmouseOut(){
+//     var x = document.getElementById("jscss");
+//     x.style.backgroundColor = "white";
+//     x.style.padding = "0rem";
+// }
+
+// function onMouseDown(){
+//     var x = document.getElementById("onmousedown");
+//     x.style.backgroundColor = "green";
+//     x.style.padding = "0.5rem 1rem";
+//     x.style.color = "white";
+// }
+
+// function onChange1(){
+//     var amount = 10;
+//     var inputAmount = document.getElementById("amt").value;
+//     var total = amount * inputAmount;
+//     document.getElementById("total").innerHTML = total;
+// }
+
+
+
+
